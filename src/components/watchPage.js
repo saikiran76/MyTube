944c1e6a3a4b7d59a2bc4eaed9b2c00885125bfb
kiran76 <ksknew76@gmail.com>
2024-04-26 20:44:23 +0530

    emded video changed

--- src/components/watchPage.js
+++ src/components/watchPage.js
@@ -10,16 +10,20 @@ import ChatSection from "./chatSection";
 const WatchPage = () =>{
     const dispatch = useDispatch();
     const [param] = useSearchParams()
-    const videoParam = param.get("v");
+    const videoParam = param.get("v"); // to get video id
     console.log(videoParam);
     useEffect(()=>{
         dispatch(closeMenu());
     }, [])
     return(
-        <div className="px-10 py-6 rounded-lg">
-            <div className="flex">
-                <div><iframe width="720" height="412" src={"https://www.youtube.com/watch?v=" + videoParam } title="YouTube video player" allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" style={{borderRadius: "1em"}}></iframe></div>
+        <div className="px-10 py-6 rounded-lg w-screen">
+            <div className="flex flex-col md:flex-row">
+                <div className="w-[80px] md:w-[720px]">
+                <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + videoParam }title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
                 <ChatSection/>
+
+                
 
             </div>
       
